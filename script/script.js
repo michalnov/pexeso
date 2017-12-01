56326269807faa942a4c4de136c48015eb0caf45
michal.novotny <michal.novotny@akademiasovy.sk>
2017-12-01 13:09:12 +0100

    initialization

---
+++ script/script.js
@@ -0,0 +1,35 @@
+var player=1;
+var field = [ [1,1,2,2],[3,3,4,4],[5,5,6,6,],[7,7,8,8] ] ;
+//console.log(field);
+shuffle();
+function shuffle() {
+	var i;
+	for (i=0;i<20;i++)
+	{
+	var r1=Math.floor( Math.random()*4 );
+	var c1=Math.floor( Math.random()*4 );
+	var r2=Math.floor( Math.random()*4 );
+	var c2=Math.floor( Math.random()*4 );
+	temp = field[r1][c1];
+	field[r1][c1]=field[r2][c2];
+	field[r2][c2]=temp;
+	}
+	console.log("Shuffle");
+	console.log(field);
+
+}
+function clickimg(value){
+	var row=Math.floor(value/10);
+	var col=value%10;
+	//console.log("You click row"+row+"col:"+col);
+	if (field[row][col]) {
+		var source="img/img"+field[row][col]+".png";
+		var imageid="img"+row+col;
+		console.log(imageid);
+		document.getElementById(imageid).src=source;
+		document.getElementById(imageid).width = 150;
+		document.getElementById(imageid).height = 150;
+
+	}
+
+}
